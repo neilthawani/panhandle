0e4c868995f2c5890989af58e69f62aff09bfd16
Neil Thawani <neilthawani@gmail.com>
2018-05-13 13:01:14 -0500

    Setup package.json initial iteration

--- lib/build.sh
+++ lib/build.sh
@@ -5,6 +5,8 @@
 echo "Building package.json"
 echo "In build.sh"
 echo `pwd`
+echo argv
+echo args
 # TODO: Figure out how to run npm in cwd/projectName
 # Also see how to catch errors, because it's halting the script here
 # npm init
