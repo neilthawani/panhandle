6a2c50ece24e48035ed7b80d2f8737bb670ffa7b
Neil Thawani <neilthawani@gmail.com>
2018-06-18 09:12:40 -0500

    Update config.js

--- blueprints/config/config.js
+++ blueprints/config/config.js
@@ -1,7 +1,9 @@
+const baseFolder = "src/";
+const sassFolder = "static/styles/";
+
 module.exports = {
-    baseFolder: "src/",
     globToMatch: {
-        partialsFolder: baseFolder + "views/partials/**/*.hbs"
+        partialsFolder: baseFolder + "views/partials/**/*.hbs",
         layoutsFolder: baseFolder + "views/layouts/**/*.hbs",
         templatesFolder: baseFolder + "views/templates/**/*.hbs"
     },
@@ -10,12 +12,8 @@ module.exports = {
         partialsFolder: baseFolder + "views/partials/",
         templatesFolder: baseFolder + "views/templates/"
     },
-
-    styles: {
-        sassFolder: "static/styles/"
-    }
     scripts: {
-        "build-css": "node-sass --include-path scss " + baseFolder + styles.sassFolder + "main.scss styles/main.css",
+        "build-css": "node-sass --include-path scss " + baseFolder + sassFolder + "main.scss styles/main.css",
         "watch-css": "nodemon -e scss -x \"npm run build-css\"",
         "watch-views": "nodemon -e hbs -x \"npm run build-views\""
     }
